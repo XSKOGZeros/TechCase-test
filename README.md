93c5a8f4340d39d7811bb76e3b808e2ae72eed01
Paulo Alves <paulo.ultimate@gmail.com>
2022-08-16 13:26:26 -0300

    Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tech Case (e-core)
+
+# EN-US
+
+## Instructions:
+
+- To run this app, you will need to clone it into your local machine and run the index.html file with any modern browser that allows javascript to run from disk 
+
+## Feature Testing:
+
+- To register a new entry, type the name and the age in the labeled inputs, the entry will automaticaly appear in the list on the right side of the screen.
+
+- The list will be automaticaly be ordered fisrt by age (crescent) and then by name (in alphabetical order).
+
+- The list will also show the category of each entry, the entries can be filtered by category using the checkboxes below the entry list.
+
+# PT-BR
+
+## Instruções:
+
+- Para rodar o aplicativo, será necessário clonar o repositório para sua maquina local e executar o arquivo index.html com qualquer browser moderno, que permita a execução de scripts do javascript direto do disco
+
+## Teste de funcionalidades:
+
+- Para adicionar um novo registro, digite o nome e a idade nos inputs indicados, o registro irá aparecer automaticamente na lista, no lado direito da tela. 
+
+- A lista será ordenada automaticamente por idade (crescente) e por nome (em ordem alfabética).
+
+- A lista também irá mostrar a categoria de cada registro, os registros podem ser filtrados por categoria utilizando os checkboxes abaixo da lista de registros. 
---
+++ js/registrationController.js
@@ -0,0 +1,80 @@
+angular.module("registrationControllerModule", [])
+
+.controller("registrationController", ["$scope", "arrayUtils" , function($scope,arrayUtils){
+
+    // Initialize the registration form
+    $scope.formParams={};
+    $scope.formParams.name="";
+    $scope.formParams.age="";
+    $scope.formParams.nameValid=true;
+    $scope.formParams.ageValid=true;
+    $scope.formParams.category = {child: true, teenager: true, adult: true, elderly:true};
+    $scope.personArray=[];
+
+    // Add a new person to the array
+    $scope.register = function(){
+        $scope.formParams.nameValid=true;
+        $scope.formParams.ageValid=true;
+        if($scope.formParams.name==""){
+            $scope.formParams.nameValid=false;
+        }
+        if(isNaN(parseInt($scope.formParams.age))){
+            $scope.formParams.ageValid=false;
+        }
+        if( $scope.formParams.nameValid && $scope.formParams.ageValid){
+            var entry = {name: $scope.formParams.name.toUpperCase(), age: parseInt($scope.formParams.age), category: $scope.getCategory($scope.formParams.age)};
+            arrayUtils.sortedInsertion($scope.personArray, entry);
+            $scope.formParams.name="";
+            $scope.formParams.age="";
+        }
+    };
+
+    // Get the category of the person
+    $scope.getCategory = function(age){
+        if(age<=11){//0-11
+            return "child";
+        }
+        else if(age<=19){//12-19
+            return "teenager";
+        }
+        else if(age<=64){//20-64
+            return "adult";
+        }
+        else{ //65+
+            return "elderly";
+        }
+    }
+
+
+
+}])
+.factory("arrayUtils", function(){
+    var arrayUtils = {};
+
+    // Compare two entries and return the result
+    arrayUtils.compareEntries = function(a, b){
+        if(a.age < b.age){
+            return -1;
+        }
+        if(a.age > b.age){
+            return 1;
+        }
+        if(a.name < b.name){
+            return -1;
+        }
+        if(a.name > b.name){
+            return 1;
+        }
+        return 0;
+    }
+
+    // Insert an entry into the sorted array
+    arrayUtils.sortedInsertion = function(array, entry){
+        var i = 0;
+        while(i < array.length && arrayUtils.compareEntries(array[i],entry) <= 0){
+            i++;
+        }
+        array.splice(i, 0, entry);
+    }
+    return arrayUtils;
+});
